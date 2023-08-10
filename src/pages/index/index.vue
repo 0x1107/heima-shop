39d1aced5e460ad249ce9c64e89d9148f41533da
Jane Thompson <339002469@qq.com>
2023-08-10 19:57:32 +0800

    轮播图接入数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import CustomNavbar from './components/CustomNavbar.vue';
+
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import CustomNavbar from './components/CustomNavbar.vue'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
+
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
   <view class="index">index</view>
 </template>
 
